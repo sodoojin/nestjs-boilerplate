45209ef5512cc020dd8c64ecbf31d56571990b91
sodoojin <visualplus@daum.net>
2022-12-26 14:23:32 +0900

    ho 토큰 인증 기능 추가

---
+++ src/modules/auth/strategies/admin.strategy.spec.ts
@@ -0,0 +1,166 @@
+import * as crypto from 'crypto';
+import jwtDecode from 'jwt-decode';
+import { when } from 'jest-when';
+import { AdminStrategy } from './admin.strategy';
+
+const createHash = {
+  update: jest.fn(),
+};
+const createHashDigestWrap = (v) => ({
+  digest: () => v,
+});
+
+jest.mock('crypto', () => ({
+  createHash: jest.fn(),
+  randomBytes: jest.fn(),
+  createCipheriv: jest.fn(),
+  createDecipheriv: jest.fn(),
+}));
+
+jest.mock('jwt-decode', () => ({
+  default: jest.fn(),
+}));
+
+describe('TokenStrategy', () => {
+  const accessToken = 'accessToken';
+  const refreshToken = 'refreshToken';
+
+  const hashedRefreshToken = 'hashedRefreshToken';
+
+  const configService = {
+    get: jest.fn(),
+  } as any;
+  const JWT_PRIVATE_KEY = 'JWT_PRIVATE_KEY';
+  const JWT_PUBLIC_KEY = 'JWT_PUBLIC_KEY';
+
+  async function business(req) {
+    // private key + refresh token 을 sha256으로 해싱 -- (a)
+    when<any, any>(crypto.createHash)
+      .calledWith('SHA256')
+      .mockReturnValue(createHash);
+    when(createHash.update)
+      .calledWith(`${JWT_PRIVATE_KEY}_${refreshToken}`)
+      .mockReturnValue(createHashDigestWrap(hashedRefreshToken));
+
+    // (a) 를 aes-256-cbc 알고리즘으로 암호화 -- (b)
+    const iv = Buffer.from('0000000000000000');
+    const cipher = {
+      update: jest.fn(),
+      final: jest.fn(),
+    };
+    const encrypted = Buffer.from('encrypted', 'utf-8');
+    const encryptedFinal = Buffer.from('final', 'utf-8');
+
+    when<Buffer, [number]>(crypto.randomBytes)
+      .calledWith(16)
+      .mockReturnValue(iv);
+    when<any, any>(crypto.createCipheriv)
+      .calledWith('aes-256-cbc', Buffer.from(JWT_PUBLIC_KEY, 'hex'), iv)
+      .mockReturnValue(cipher);
+    when(cipher.update)
+      .calledWith(hashedRefreshToken)
+      .mockReturnValue(encrypted);
+    when(cipher.final).mockReturnValue(encryptedFinal);
+
+    // (b) 를 aes-256-bcb 알고리즘으로 복호화 -- (c)
+    const decipher = {
+      update: jest.fn(),
+      final: jest.fn(),
+    };
+    const decrypted = Buffer.from('decrypted', 'utf-8');
+    const decryptedFinal = Buffer.from('decryptedFinal', 'utf-8');
+    when<any, any>(crypto.createDecipheriv)
+      .calledWith('aes-256-cbc', Buffer.from(JWT_PUBLIC_KEY, 'hex'), iv)
+      .mockReturnValue(decipher);
+    when(decipher.update)
+      .calledWith(Buffer.concat([encrypted, encryptedFinal]))
+      .mockReturnValue(decrypted);
+    when(decipher.final).mockReturnValue(decryptedFinal);
+
+    // (c) 를 사용하여 access token 을 aes-256-ecb 알고리즘으로 복호화
+    const hashedDecrypted = 'hashedDecrypted';
+    const accessTokenDecipher = {
+      update: jest.fn(),
+      final: jest.fn(),
+    };
+    const accessTokenDecrypted = Buffer.from('accessTokenDecrypted', 'utf-8');
+    const accessTokenFinal = Buffer.from('accessTokenFinal', 'utf-8');
+
+    when<any, any>(crypto.createHash)
+      .calledWith('SHA256')
+      .mockReturnValue(createHash);
+    when(createHash.update)
+      .calledWith(
+        Buffer.concat([decrypted, decryptedFinal, Buffer.from('_', 'utf-8')]),
+      )
+      .mockReturnValue(createHashDigestWrap(hashedDecrypted));
+    when<any, any>(crypto.createDecipheriv)
+      .calledWith('aes-256-ecb', hashedDecrypted, '')
+      .mockReturnValue(accessTokenDecipher);
+    when(accessTokenDecipher.update)
+      .calledWith(Buffer.from(accessToken, 'base64'))
+      .mockReturnValue(accessTokenDecrypted);
+    when(accessTokenDecipher.final).mockReturnValue(accessTokenFinal);
+
+    // jwt decode
+    const jwt = {
+      id: 'id',
+      aud: 'aud',
+      sub: 'user id',
+      exp: 1671756470,
+      iat: 1671755270,
+      token_type: 'bearer',
+      scope: 'scope',
+    };
+    when(jwtDecode)
+      .calledWith(
+        Buffer.concat([accessTokenDecrypted, accessTokenFinal]).toString(
+          'utf-8',
+        ),
+      )
+      .mockReturnValue(jwt);
+
+    const strategy = new AdminStrategy(configService);
+    const result = await strategy.validate(req as any);
+
+    expect(result).toBe(jwt);
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    when(configService.get)
+      .calledWith('JWT_PRIVATE_KEY')
+      .mockReturnValue(JWT_PRIVATE_KEY);
+
+    when(configService.get)
+      .calledWith('JWT_PUBLIC_KEY')
+      .mockReturnValue(JWT_PUBLIC_KEY);
+  });
+
+  it('header 에 저장', async () => {
+    // request 세팅
+    const req = {
+      header: jest.fn(),
+    };
+    when(req.header).calledWith('biz_atk').mockReturnValue(accessToken);
+    when(req.header).calledWith('biz_rtk').mockReturnValue(refreshToken);
+
+    await business(req);
+  });
+
+  it('cookie 에 저장', async () => {
+    // request 세팅
+    const req = {
+      header: jest.fn(),
+      cookies: {
+        biz_atk: accessToken,
+        biz_rtk: refreshToken,
+      },
+    };
+    when(req.header).calledWith('biz_atk').mockReturnValue(null);
+    when(req.header).calledWith('biz_rtk').mockReturnValue(null);
+
+    await business(req);
+  });
+});
---
+++ src/modules/auth/strategies/admin.strategy.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-custom';
+import { Request } from 'express';
+import * as crypto from 'crypto';
+import jwtDecode from 'jwt-decode';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AdminStrategy extends PassportStrategy(Strategy, 'admin') {
+  constructor(private configService: ConfigService) {
+    super();
+  }
+
+  async validate(req: Request) {
+    let accessToken, refreshToken;
+
+    if (req.header('biz_atk') && req.header('biz_rtk')) {
+      accessToken = req.header('biz_atk');
+      refreshToken = req.header('biz_rtk');
+    } else {
+      accessToken = req.cookies['biz_atk'];
+      refreshToken = req.cookies['biz_rtk'];
+    }
+
+    return jwtDecode(
+      this.decryptAccessToken(
+        accessToken,
+        this.decrypt(
+          this.encrypt(
+            crypto
+              .createHash('SHA256')
+              .update(
+                `${this.configService.get('JWT_PRIVATE_KEY')}_${refreshToken}`,
+              )
+              .digest(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  private encrypt(text) {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(
+      'aes-256-cbc',
+      Buffer.from(this.configService.get('JWT_PUBLIC_KEY') as string, 'hex'),
+      iv,
+    );
+    let encrypted = cipher.update(text);
+
+    encrypted = Buffer.concat([iv, encrypted, cipher.final()]);
+
+    return encrypted;
+  }
+
+  private decrypt(buffer) {
+    const iv = buffer.slice(0, 16);
+
+    const decipher = crypto.createDecipheriv(
+      'aes-256-cbc',
+      Buffer.from(this.configService.get('JWT_PUBLIC_KEY') as string, 'hex'),
+      iv,
+    );
+
+    let decrypted = decipher.update(buffer.slice(16, buffer.length));
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted;
+  }
+
+  private decryptAccessToken(token, key) {
+    const message = Buffer.from(token, 'base64');
+    const decipher = crypto.createDecipheriv(
+      'aes-256-ecb',
+      crypto
+        .createHash('SHA256')
+        .update(Buffer.concat([key, Buffer.from('_', 'utf-8')]))
+        .digest(),
+      '',
+    );
+
+    let decrypted = decipher.update(message);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted.toString('utf-8');
+  }
+}
