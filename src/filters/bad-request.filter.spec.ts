6335fc240708cea02f4d8e9cc184b9dc671d47e1
sodoojin <visualplus@daum.net>
2023-01-06 10:02:02 +0900

    node-mocks-http 적용

--- src/filters/bad-request.filter.spec.ts
+++ src/filters/bad-request.filter.spec.ts
@@ -1,73 +1,97 @@
 import { BadRequestFilter } from './bad-request.filter';
 import { BadRequestException, HttpStatus } from '@nestjs/common';
-import { when } from 'jest-when';
+import * as httpMocks from 'node-mocks-http';
 
 describe('BadRequestExceptionFilter', () => {
   const errorMessages = {
     name: ['이름을 입력해 주세요.'],
     age: ['나이를 입력해 주세요.', '나이는 숫자만 입력 가능합니다.'],
   };
-  const header = jest.fn();
-  const status = jest.fn();
-  const flash = jest.fn();
-  const redirect = jest.fn();
-  const send = jest.fn();
   const badRequestFilter = new BadRequestFilter();
   const badRequestException = new BadRequestException({
     statusCode: HttpStatus.BAD_REQUEST,
     message: errorMessages,
     error: 'Bad Request',
   });
-  const body = 'request-body';
-  const referer = 'request-referer';
-  let argumentsHost;
 
-  beforeEach(() => {
+  afterEach(() => {
     jest.clearAllMocks();
-
-    argumentsHost = {
-      switchToHttp: () => ({
-        getRequest: () => ({
-          header,
-          flash,
-          body,
-          headers: {
-            referer,
-          },
-        }),
-        getResponse: () => ({
-          redirect,
-          status,
-          send,
-        }),
-      }),
-    };
   });
 
   it('ajax 호출에서 exception 발생', () => {
-    when(header)
-      .calledWith('x-requested-with')
-      .mockReturnValueOnce('XMLHttpRequest');
+    const { req, res } = httpMocks.createMocks({
+      headers: {
+        'x-requested-with': 'XMLHttpRequest',
+      },
+    });
 
-    when(status)
-      .calledWith(400)
-      .mockReturnValue(argumentsHost.switchToHttp().getResponse());
+    badRequestFilter.catch(badRequestException, {
+      switchToHttp: () => ({
+        getRequest: () => req,
+        getResponse: () => res,
+      }),
+    } as any);
 
-    badRequestFilter.catch(badRequestException, argumentsHost);
-
-    expect(send).toHaveBeenCalledWith({
+    expect(res.statusCode).toEqual(400);
+    expect(res._getData()).toEqual({
       status: 'fail',
       data: errorMessages,
     });
   });
 
-  it('일반 request 호출에서 exception 발생', () => {
-    when(header).calledWith('x-requested-with').mockReturnValueOnce('');
+  it('일반 request 호출에서 exception 발생 referer 없음', () => {
+    const flash = jest.fn();
+    const body = {
+      req1: 'req1',
+      req2: 'req2',
+    };
+    const { req, res } = httpMocks.createMocks(
+      {
+        flash,
+        body,
+      },
+      {},
+    );
 
-    badRequestFilter.catch(badRequestException, argumentsHost);
+    badRequestFilter.catch(badRequestException, {
+      switchToHttp: () => ({
+        getRequest: () => req,
+        getResponse: () => res,
+      }),
+    } as any);
 
     expect(flash).toHaveBeenCalledWith('validationErrors', errorMessages);
     expect(flash).toHaveBeenCalledWith('userOldInputValues', body);
-    expect(redirect).toHaveBeenCalledWith(referer);
+    expect(res._getRedirectUrl()).toEqual('/');
+  });
+
+  it('일반 request 호출에서 exception 발생 referer 있음', () => {
+    const flash = jest.fn();
+    const body = {
+      req1: 'req1',
+      req2: 'req2',
+    };
+    const referer = 'https://example.com/a/b/c';
+    const { req, res } = httpMocks.createMocks(
+      {
+        headers: {
+          referer,
+        },
+        flash,
+        body,
+      },
+      {},
+    );
+
+    badRequestFilter.catch(badRequestException, {
+      switchToHttp: () => ({
+        getRequest: () => req,
+        getResponse: () => res,
+      }),
+    } as any);
+
+    expect(flash).toHaveBeenCalledWith('validationErrors', errorMessages);
+    expect(flash).toHaveBeenCalledWith('userOldInputValues', body);
+    expect(res._getRedirectUrl()).toEqual(referer);
   });
 });
